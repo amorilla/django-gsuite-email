5d9cf9a24b23e6266fb179881e71ed3728c48280
Antonio Morillas <amorilla@xtec.cat>
2024-04-19 20:15:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ URL = 'https://github.com/slicefox/django-gsuite-email'
 EMAIL = 'adithya.webdev@gmail.com'
 AUTHOR = 'slicefox'
 REQUIRES_PYTHON = '>=3.0.0'
-VERSION = '0.1.2'
+VERSION = '0.1.3'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
