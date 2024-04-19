53550a8fae09d9d6c5c89f8932e30282b2df7632
Antonio Morillas <amorilla@xtec.cat>
2024-04-19 12:39:19 +0200

    Fix open connection and recipients bugs

--- django_gsuite_email/backends.py
+++ django_gsuite_email/backends.py
@@ -3,6 +3,7 @@ import threading
 
 from django.conf import settings
 from django.core.mail.backends.base import BaseEmailBackend
+from django.core.mail.backends.smtp import EmailBackend
 from django.core.mail.message import sanitize_address
 from google.auth import exceptions
 from google.oauth2 import service_account
@@ -14,13 +15,16 @@ from .utils import get_credentials_file
 class GSuiteEmailBackend(BaseEmailBackend):
     def __init__(self, fail_silently=False, **kwargs):
 
+        super().__init__(fail_silently=fail_silently)
+        
         self.fail_silently = fail_silently
         self.credentials = get_credentials_file()
         self.API_SCOPE = ['https://www.googleapis.com/auth/gmail.send', ]
-        # to reopen connection with different delegation when from_email changes
+        # to reopen connection with different delegation when user changes
         self.current_user = None
         self.connection = None
         self._lock = threading.RLock()
+        self.gmail_user = settings.GMAIL_USER
 
     def _delegate_user(self, user_id):
         credentials = service_account.Credentials.from_service_account_file(
@@ -38,21 +42,20 @@ class GSuiteEmailBackend(BaseEmailBackend):
         with self._lock:
             num_sent = 0
             for message in email_messages:
-                # maybe sanitize this message.from_email?
-                new_conn_created = self.open(from_email=message.from_email)
+                encoding = message.encoding or settings.DEFAULT_CHARSET
+                self.gmail_user = sanitize_address(message.from_email, encoding)
+                self.gmail_user = self.gmail_user.split(" ")[1]
+                self.gmail_user = self.gmail_user[1:len(self.gmail_user)-1]
+                new_conn_created = self.open()
                 if not self.connection or new_conn_created is None:
                     # skip this message
                     continue
-
                 sent = self._send(message)
                 if sent:
                     num_sent += 1
-            # not doing anything here
-            # if new_conn_created:
-            #     self.close()
         return num_sent
 
-    def open(self, from_email):
+    def open(self):
         """
         Ensure an open connection to the email server. Return whether or not a
         new connection was required (True or False) or None if an exception
@@ -60,19 +63,21 @@ class GSuiteEmailBackend(BaseEmailBackend):
         """
         if not self.current_user:
             # first connection
-            self.current_user = from_email
+            self.current_user = self.gmail_user
 
-        if self.connection and from_email == self.current_user:
+        if self.connection and self.gmail_user == self.current_user:
             # Nothing to do if the connection is already open for same delegation
             return False
 
         try:
-            credentials = self._delegate_user(from_email)
+            self.close()
+            self.current_user = self.gmail_user
+            credentials = self._delegate_user(self.current_user)
             self.connection = build("gmail", "v1", credentials=credentials)
-            self.current_user = from_email
             return True
         except (exceptions.DefaultCredentialsError, exceptions.GoogleAuthError, exceptions.RefreshError, exceptions.TransportError):
             if not self.fail_silently:
+                self.close()
                 raise
 
     def close(self):
@@ -80,18 +85,21 @@ class GSuiteEmailBackend(BaseEmailBackend):
         if self.connection is None:
             return
         # do something
+        self.connection.close()
+        self.connection = None
+        self.current_user = None
         return
 
     def _send(self, email_message):
         """A helper method that does the actual sending."""
         if not email_message.recipients():
             return False
-        encoding = email_message.encoding or settings.DEFAULT_CHARSET
         # check this
-        from_email = sanitize_address(email_message.from_email, encoding)
-        recipients = [sanitize_address(addr, encoding)
-                      for addr in email_message.recipients()]
         message = email_message.message()
+        print("gsuite 1: "+str(message['from']))
+        if email_message.bcc:
+            message['bcc'] = ','.join(map(str, email_message.bcc))
+        print("gsuite 2: "+str(message))
         # https://developers.google.com/gmail/api/guides/sending#creating_messages
         raw = base64.urlsafe_b64encode(message.as_bytes())
         raw = raw.decode()
@@ -99,8 +107,7 @@ class GSuiteEmailBackend(BaseEmailBackend):
         try:
             # need different login to check success
             self.connection.users().messages().send(
-                userId='me', body=binary_content).execute()
-            # self.connection.sendmail(from_email, recipients, message.as_bytes(linesep='\r\n'))
+                userId=self.gmail_user, body=binary_content).execute()
         except (exceptions.DefaultCredentialsError, exceptions.GoogleAuthError, exceptions.RefreshError, exceptions.TransportError):
             if not self.fail_silently:
                 raise
